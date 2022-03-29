c8ea03c49fe223760b28eb608d7f598969ca450f
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-03-29 11:58:49 +0530

    enquiry seed updated

---
+++ database/migrations/2022_03_29_121741_create_enquiries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnquiriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enquiries', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("mobile");
+            $table->string("email");
+            $table->string("requirement")->nullable();
+            $table->boolean("is_active")->default(1);
+            $table->softDeletes('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enquiries');
+    }
+}
