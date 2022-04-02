1f15c2dd34563fe65ab57b66503737eecdc4f7a3
vetri-pixel <vetriselvam.pixel@gmail.com>
2022-04-02 14:58:09 +0530

    customer issue fixing

--- app/Customer.php
+++ app/Customer.php
@@ -2,12 +2,13 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
     protected $fillable =[
-        "customer_group_id", "user_id", "name", "company_name",
+        "customer_group_id", "user_id", "name", "company_name","walk-in-customer","full_name","last_visited",
         "email", "phone_number","requirement","tax_no", "address", "city",
         "state", "postal_code", "country", "points", "deposit", "expense", "is_active"
     ];
@@ -24,7 +25,28 @@ class Customer extends Model
 
     public function payments()
     {
-        return $this->hasMany(Payment::class);
+        return $this->hasMany(Payment::class,'customer_id','id');
     }
+
+    public function CustomerGroup()
+    {
+        return $this->belongsTo(CustomerGroup::class,'customer_group_id','id');
+    }
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format(config('global.model_date_format'));
+    }
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format(config('global.model_date_format'));
+    }
+
+    public function getLastVisitedAttribute($date)
+    {
+        return Carbon::parse($date)->format(config('global.model_date_format'));
+    }
+
  
 }
